1500f05594c3a02f1fba3cac5b59895f60b01d61
135ahmedsayed <ahmedshkra246@gmail.com>
2025-06-25 08:43:40 +0300

    Q11 solve

--- Assignment_02/Program.cs
+++ Assignment_02/Program.cs
@@ -131,6 +131,36 @@
             Console.WriteLine("Percentage is : " + $"{Percentage}");
             Console.WriteLine("\n");
             #endregion
+            #region Q11
+            int Month;
+            Console.Write("Enter Month Number : ");
+            Month = Convert.ToInt32(Console.ReadLine());
+            switch (Month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    Console.WriteLine("Days in Month: 31");
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    Console.WriteLine("Days in Month: 30");
+                    break;
+                case 2:
+                    Console.WriteLine("Days in Month: 28");
+                    break;
+                default:
+                    Console.WriteLine("That is not Month");
+                    break;
+            }
+            Console.WriteLine("\n");
+            #endregion
             Console.ReadKey();
         }
     }
