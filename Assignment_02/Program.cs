447dbce19d92fa8489dd20dd0c0074865bd5b96d
135ahmedsayed <ahmedshkra246@gmail.com>
2025-06-25 08:36:33 +0300

    Q1 solve

--- Assignment_02/Program.cs
+++ Assignment_02/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            int Number;
+            Console.Write("Enter Number : ");
+            Number = Convert.ToInt32(Console.ReadLine());
+            if (Number % 3 == 0 && Number % 4 == 0)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+            #endregion
+            Console.ReadKey();
         }
     }
 }
